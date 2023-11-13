c53c1253ea4b4e158c7e6745df4fee342a197098
joyylyu <lvchenqujoy@126.com>
2023-11-13 17:25:16 +0000

    corrected bugs

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ Before running the scripts its required to load the following modules:
 ```
 module load BioPerl/1.7.8-GCCcore-11.3.0
 module load Biopython/1.79-foss-2022a
-
 ```
 
 
--- predict_eff.sh
+++ predict_eff.sh
@@ -7,7 +7,7 @@
 tool_path=$(jq -r '.tool_path' config.json)
 proteome_path=$(jq -r '.proteome_path' config.json)
 output_path=$(jq -r '.output_path' config.json)
-sample=($(jq -r '.samples' config.json))
+sample=($(jq -r '.samples[]' config.json))
 
 tool_path="${tool_path%/}"
 output_path="${output_path%/}"
