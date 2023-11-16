e9e3ef1c6b9490b5b8e4503310f6593257fa1bbc
joyylyu <lvchenqujoy@126.com>
2023-11-16 12:21:29 +0000

    tm added

--- predict_eff.sh
+++ predict_eff.sh
@@ -16,16 +16,28 @@ for name in "${sample_no[@]}"; do
    mkdir -p "${output_path}/${species}/${name}"
    biolib run DTU/SignalP_6 --fastafile "${proteome_path}/fusarium_lateritium_${sample_no}.proteins.fa" \
                             --output_dir "${output_path}/${species}/${name}"
+    secreted_pro=($(awk '!/^#/ {print $1}' "${output_path}/${species}/${name}/biolib_results/output.gff3"))
+    grep -Fwf <(printf "%s\n" "${secreted_pro[@]}") -A 1 "${proteome_path}/fusarium_lateritium_${name}.proteins.fa" > "${output_path}/${name}/${name}_SSP.fasta"
+
 done 
 
-secreted_pro=($(awk '!/^#/ {print $1}' "${output_path}/${species}/${name}/biolib_results/output.gff3"))
-se creted_seq=($(grep -Fwf <(printf "%s\n" "${secreted_pro[@]}") -A 1 "${proteome_path}/fusarium_lateritium_${name}.proteins.fa"))
-secreted_sample=($(grep '>' "$secreted_seq[@]"))
+#predict transmembrane domain
+for name in "${sample_no[@]}"; do 
+    cat ${output_path}/${name}/${name}_SSP.fasta  | \
+    ${tool_path}/tmhmm/decodeanhmm.Linux_x86_64 -f ${tool_path}/tmhmm/TMHMM2.0.options \
+    -modelfile ${tool_path}/tmhmm/TMHMM2.0c.model > ${output_path}/${name}/tmhmm.gff3
+    perl ${tool_path}/tmhmm/tmhmmformat.pl \
+    ${output_path}/${name}/tmhmm.gff3 > ${output_path}/${name}/res.gff3
+    grep -E "Number of predicted TMHs:  0" ${output_path}/${name}/res.gff3| sed 's/# />/g' > ${output_path}/${name}/IDtmhmm.txt
+    cut -d ' ' -f1 ${output_path}/${name}/IDtmhmm.txt > ${output_path}/${name}/id.wotransmembrandomains.fastaxs
+    grep -Fwf id.wotransmembrandomains.fastaxsi -A 1 "${output_path}/${name}/${name}_SSP.fasta" > "${output_path}/${name}/${name}_NOTM.fasta"
+done
+
 
 # predict effectors
 for name in "${secreted_sample[@]}"; do
     python ${tool_path}/EffectorP-3.0/EffectorP.py \
-    -f -i "${output_path}/${name}/${name}_SSP.fasta" \
+    -f -i "${output_path}/${name}/${name}_NOTM.fasta" \
     -o "${output_path}/${name}/${name}_EFF.txt" \
     -E "${output_path}/${name}/${name}_EFF.fasta"
 done 
