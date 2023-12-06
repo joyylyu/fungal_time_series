ea998fe1ee7078fc24402c2977d591278d5c388c
joyylyu <lvchenqujoy@126.com>
2023-12-06 18:36:33 +0000

    change directories

--- predict_eff.sh
+++ predict_eff.sh
@@ -5,7 +5,7 @@
 
 #SBATCH --job-name=predict_effectors
 #SBATCH --time=1-12:00:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem=8g
 
@@ -21,33 +21,35 @@ sample_no=("${sample[@]//[^0-9]/}")
 # predict SSPs
 for name in "${sample_no[@]}"; do 
    mkdir -p "${output_path}/${species}/${sample}"
+   mkdir -p "${output_path}/${species}/${sample}/SignalP6"
    signalp6 --fastafile "${proteome_path}/${full_sp_name}_${name}.proteins.fa" \
-           --output_dir "${output_path}/${species}/${sample}"  \
-           --model_dir "${tool_path}/signalp-6-package/models/}" \ 
+           --output_dir "${output_path}/${species}/${sample}/SignalP6"  \
+           --model_dir "${tool_path}/signalp-6-package/models" \
            --format none 
-    secreted_pro=($(awk '!/^#/ {print $1}' "${output_path}/${species}/${sample}/biolib_results/output.gff3"))
-    grep -Fwf <(printf "%s\n" "${secreted_pro[@]}") -A 1 "${proteome_path}/${full_sp_name}_${name}.proteins.fa" > "${output_path}/${name}/${name}_SSP.fasta"
-
+    secreted_pro={$(awk '!/^#/ {print $1}' "${output_path}/${species}/${sample}/SignalP6/"output.gff3)}
+    grep -Fwf <(printf "%s\n" "${secreted_pro[@]}") -A 1 "${proteome_path}/${full_sp_name}_${name}.proteins.fa" > "${output_path}/${species}/${sample}/SignalP6/${sample}_SSP.fasta"
+    sed -i 's/--//g' "${output_path}/${species}/${sample}/SignalP6/${sample}_SSP.fasta" 
 done 
 
 #predict transmembrane domain
-for name in "${sample_no[@]}"; do 
-    cat ${output_path}/${name}/${name}_SSP.fasta  | \
+for name in "${sample_no[@]}"; do
+    mkdir -p "${output_path}/${species}/${sample}/TMHMM"
+    cat ${output_path}/${species}/${sample}/SignalP6/${sample}_SSP.fasta  | \
     ${tool_path}/tmhmm/decodeanhmm.Linux_x86_64 -f ${tool_path}/tmhmm/TMHMM2.0.options \
-    -modelfile ${tool_path}/tmhmm/TMHMM2.0c.model > ${output_path}/${name}/tmhmm.gff3
+    -modelfile ${tool_path}/tmhmm/TMHMM2.0c.model > ${output_path}/${species}/${sample}/TMHMM/tmhmm.gff3
     perl ${tool_path}/tmhmm/tmhmmformat.pl \
-    ${output_path}/${name}/tmhmm.gff3 > ${output_path}/${name}/res.gff3
-    grep -E "Number of predicted TMHs:  0" ${output_path}/${name}/res.gff3| sed 's/# />/g' > ${output_path}/${name}/IDtmhmm.txt
-    cut -d ' ' -f1 ${output_path}/${name}/IDtmhmm.txt > ${output_path}/${name}/id.wotransmembrandomains.fastaxs
-    grep -Fwf id.wotransmembrandomains.fastaxsi -A 1 "${output_path}/${name}/${name}_SSP.fasta" > "${output_path}/${name}/${name}_NOTM.fasta"
+    ${output_path}/${species}/${sample}/TMHMM/tmhmm.gff3 > ${output_path}/${species}/${sample}/TMHMM/res.gff3
+    grep -E "Number of predicted TMHs:  0" ${output_path}/${species}/${sample}/TMHMM/res.gff3| sed 's/# />/g' > ${output_path}/${species}/${sample}/TMHMM/IDtmhmm.txt
+    cut -d ' ' -f1 ${output_path}/${species}/${sample}/TMHMM/IDtmhmm.txt > ${output_path}/${species}/${sample}/TMHMM/id.wotransmembrandomains.fastaxs
+    grep -Fwf ${output_path}/${species}/${sample}/TMHMM/id.wotransmembrandomains.fastaxs -A 1 "${output_path}/${species}/${sample}/SignalP6/${sample}_SSP.fasta" > "${output_path}/${species}/${sample}/TMHMM/${sample}_NOTM.fasta"
 done
 
 
 # predict effectors
 for name in "${secreted_sample[@]}"; do
     python ${tool_path}/EffectorP-3.0/EffectorP.py \
-    -f -i "${output_path}/${name}/${name}_NOTM.fasta" \
-    -o "${output_path}/${name}/${name}_EFF.txt" \
-    -E "${output_path}/${name}/${name}_EFF.fasta"
+    -f -i "${output_path}/${species}/${sample}/TMHMM/${sample}_NOTM.fasta" \
+    -o "${output_path}/${species}/${sample}/${sample}_EFF.txt" \
+    -E "${output_path}/${species}/${sample}/${sample}_EFF.fasta"
 done 
 
