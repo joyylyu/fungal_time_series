e5f9274c12f670898c826a86671275ca3fb99803
joyylyu <lvchenqujoy@126.com>
2024-01-02 19:54:14 +0000

    gene count done

--- prep_gene_count.sh
+++ prep_gene_count.sh
@@ -11,7 +11,7 @@ ortho_table=$(find "$data_path"/"$species"_ortho -type d -name "Results_*" -exec
 orthogroup=()
 for name in "${sample[@]}"; do
     effectors=$(awk '{print $1}' "${output_path}/${species}/${name}/${name}_EFF.txt")
-    for eff in $effectors; do 
+    for eff in ${effectors[@]}; do 
         og=$(grep -w $eff "$ortho_table" | cut -f1 )
         if  [[ ! " ${orthogroup[@]} " =~ " ${og} " ]]; then
             orthogroup+=("$og")
@@ -20,9 +20,10 @@ for name in "${sample[@]}"; do
 done
 
 ortho_count=$(find "$data_path"/"$species"_ortho -type d -name "Results_*" -exec echo {}/Orthogroups/Orthogroups.GeneCount.tsv \;)
-mkdir "$output_path"/"$species"/eff_ortho 
-awk -v orthogroup="${orthogroup[*]}" '{for (i=1; i<=NF; i++) if ($i == orthogroup) print}' "$ortho_count" > \
-"$output_path"/"$species"/eff_ortho/"$species"_EFF_count.tsv
+mkdir -p "$output_path"/"$species"/eff_ortho 
+for pattern in "${orthogroup[@]}"; do
+    grep -F "$pattern" "$ortho_count"
+done > "$output_path"/"$species"/eff_ortho/"$species"_EFF_count.tsv
 
 
 
